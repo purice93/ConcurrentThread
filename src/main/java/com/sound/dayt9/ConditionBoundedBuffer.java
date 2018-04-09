d4b6c79f06af13158ad61d2d88901c5a7860fff7
purice93 <530337704@qq.com>
2018-04-09 23:08:26 +0800

    Java并发编程原理与实战31-线程之间的通信wait-notify-All； condition-await-signal-signalAll

--- src/main/java/com/sound/dayt9/ConditionBoundedBuffer.java
+++ src/main/java/com/sound/dayt9/ConditionBoundedBuffer.java
@@ -11,6 +11,10 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ConditionBoundedBuffer {
     private Lock lock = new ReentrantLock();
+
+    /**
+     * 这里使用生产者-消费者模式
+     */
     private Condition product = lock.newCondition();
     private Condition consume = lock.newCondition();
     private final int max = 10;
@@ -21,9 +25,15 @@ public class ConditionBoundedBuffer {
         try {
             while (count >= max) {
                 System.out.println(Thread.currentThread().getName()+"生产过量，停止生产...");
+                /**
+                 * 1、生产过量，停止生产：生产者等待
+                 */
                 product.await();
             }
             count++;
+            /**
+             * 2、唤醒消费者
+             */
             consume.signal();
             System.out.println(Thread.currentThread().getName()+"生产-库存变为："+count);
         } finally {
@@ -36,10 +46,16 @@ public class ConditionBoundedBuffer {
         try {
             while (count == 0) {
                 System.out.println(Thread.currentThread().getName()+"库存为空，无法购买...");
+                /**
+                 * 3、库存为空，无法购买：消费者等待
+                 */
                 consume.await();
             }
             count--;
             System.out.println(Thread.currentThread().getName()+"消费-库存还剩："+count);
+            /**
+             * 4、唤醒生产者
+             */
             product.signal();
         } finally {
             lock.unlock();
